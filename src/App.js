60b88d074cf46ad33d0126956f5ebc927681402f
Pragati-rathore <pragatirathore511@gmail.com>
2022-11-25 17:02:12 +0530

    created order button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Header from './components/header';
 import SideBar from './components/sidebar';
 import Footer from './components/footer';
+import CreateOrderButton from './components/createOrderButton';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CreateOrderButton/>
       <SideBar/>
       <Footer/>
       
