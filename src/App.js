d3937ea70523aadf9bd9cda7e72b10e2132b0f44
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-27 13:02:32 +0530

    added pastorder create order routing

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import "./App.css";
 import NavbarAndFooter from "./components/NavbarAndFooter";
 import Signin from "./components/Auth/Signin/Signin";
 import Register from "./components/Auth/Register/Register";
-
+import Home from "./components/Home/home";
+import PastOrder from "./components/PastOrders/pastOrder";
 //import SideBar from "./components/Home/sidebar";
 //import Footer from "./components/Home/footer";
 //import CreateOrderButton from "./components/createOrderButton";
 import CreateOrder from "./components/createOrder/CreateOrder";
 
-// import Home from './components/Home/home';
-
 // import "./components/summary.css"
-//import PastOrder from "./components/PastOrders/pastOrder";
-// import PastSummary from './components/PastOrders/pastSummary';
-
+ import PastSummary from './components/PastOrders/pastSummary';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NavbarAndFooter/>}>
-            <Route index element={<Signin/>}/>
-            <Route path="register" element={<Register/>}/>
-            <Route path="createorder" element={<CreateOrder/>}/>
+          <Route path="/" element={<NavbarAndFooter />}>
+            <Route index element={<Signin />} />
+            <Route path="register" element={<Register />} />
+            <Route path="/orders" element={<Home />}>
+              <Route index element={<PastOrder />} />
+              <Route path="new" element={<CreateOrder />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
