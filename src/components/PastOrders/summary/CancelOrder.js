06b1ce9fdd2ea8c4e36d0fa1501b029b8b6a7443
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-28 01:38:27 +0530

    implementing delete order

--- src/components/PastOrders/summary/CancelOrder.js
+++ src/components/PastOrders/summary/CancelOrder.js
@@ -1,12 +1,30 @@
+import { useNavigate } from "react-router-dom";
 import "./CancelOrder.css";
 
 export default function CancelOrder(props) {
   const { cancelPopupHandler } = props;
   const orderId = props.orderId;
+  const navigate = useNavigate();
+
+  const handleDelete = (_e) => {
+    fetch("https://laundry-server.onrender.com/orders", {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("laundry-token")}`,
+      },
+      body: JSON.stringify({orderId})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <div
-        className="order-confirm-wrapper"
+        className="order-confirm-wrapper2"
         style={{
           position: "absolute",
           top: 0,
@@ -19,16 +37,24 @@ export default function CancelOrder(props) {
           alignItems: "center",
         }}
       >
-        <div className="order-success">
-          <div className="cancel-header">
+        <div className="order-success2">
+          <div className="cancel-header2">
             <div>Alert</div>
-            <div onClick={(e) => cancelPopupHandler()} id="close-cancel-popup">X</div>
+            <div onClick={(e) => cancelPopupHandler()} id="close-cancel-popup2">
+              X
+            </div>
           </div>
-          <div id="cancel-popup-text">
+          <div id="cancel-popup-text2">
             Are you sure want to cancel the order No.{" "}
             {orderId.substring(orderId.length - 4)}
           </div>
-          <button type="button" onClick={(_e) => {}}>
+          <button
+            type="button"
+            onClick={(e) => {
+              handleDelete(e);
+              navigate("/orders");
+            }}
+          >
             Proceed
           </button>
         </div>
