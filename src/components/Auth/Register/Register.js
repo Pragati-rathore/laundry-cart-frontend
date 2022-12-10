6a635922cd74a0f614ecb9d6103284e00d24c031
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-12-10 14:53:32 +0530

    deleted old register html starting from scratch

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -5,6 +5,14 @@ import { useNavigate } from "react-router-dom";
 import FooterSend from "../Footersend/Foter";
 import BACKEND_URL from "../../../exports";
 
+const errorMsg = {
+  email: "Type a Valid Email",
+  name: "Type Valid Full Name",
+  phone: "Type a 10 digit valid phone number",
+  password: "Type a valid password",
+  pincode: "Type a valid 6 digit pincode",
+};
+
 const Register = () => {
   const [user, setUser] = useState({
     name: "",
@@ -16,6 +24,12 @@ const Register = () => {
     address: "",
     password: "",
   });
+
+  const [isInvalid, setIsInvalid] = useState({
+    bool: false,
+    message: "",
+  });
+
   const navigate = useNavigate();
 
   const handleInputs = (e) => {
@@ -23,10 +37,12 @@ const Register = () => {
     let value = e.target.value;
     setUser({ ...user, [name]: value });
   };
+
   const postData = (e) => {
     e.preventDefault();
     const { name, email, phone, district, state, pincode, address, password } =
       user;
+
     fetch(`${BACKEND_URL}/register`, {
       method: "POST",
       headers: {
@@ -61,157 +77,60 @@ const Register = () => {
 
   return (
     <>
-      <div className="flex-wrapper">
-      <div className="signup-cont">
-        <div className="signup-cont-left">
-          <h1 className="signup-main-laundryheading">Laundry Service</h1>
-          <p className="signup-main-laundrydescription">
-            Doorstep Wash & Dryclean Service
-          </p>
-          <p className="signup-main-alreadyhaveaccount">Already Have Account</p>
-          <button className="signup-main-registerbutton" onClick={e => navigate("/")}>Sign In</button>
+      <div className="register-page">
+        <div className="register-page-left">
+          <div>
+            <p>Laundry Cart</p>
+            <p>Doorstep Wash & Dryclean Service</p>
+          </div>
+          <div>
+            <p>Already have an account</p>
+            <button
+              className="reg-btn light"
+              type="button"
+              onClick={(_e) => navigate("/")}
+            >
+              Sign In
+            </button>
+          </div>
         </div>
-        <div className="signup-cont-right">
-          <p className="signup-main-registerheading">REGISTER</p>
-          <div className="register-input">
-            <div className="register-input-width50">
-              <label className="custom-field two">
-                <form method="POST">
-                  <input
-                    type="text"
-                    name="name"
-                    value={user.name}
-                    onChange={handleInputs}
-                    placeholder="Name"
-                    style={{ fontSize: "18px", color: "#77838F" }}
-                  />
-                </form>
-              </label>
-              <br></br>
-              <p id="errorname"></p>
-
-              <label className="custom-field two">
-                <input
-                  type="text"
-                  name="phone"
-                  value={user.phone}
-                  onChange={handleInputs}
-                  placeholder="Phone No"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <p id="errorphone"></p>
-              <br></br>
-
-              <label className="custom-field two">
-                <input
-                  type="text"
-                  name="state"
-                  value={user.state}
-                  onChange={handleInputs}
-                  placeholder="State"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <p id="errorstate"></p>
-              <br></br>
-
-              <label className="custom-field two">
-                <input
-                  type="text"
-                  name="address"
-                  value={user.address}
-                  onChange={handleInputs}
-                  placeholder="Address"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <p id="erroraddress"></p>
-            </div>
-            <div className="register-input-width50">
-              <label className="custom-field two">
-                <input
-                  type="email"
-                  name="email"
-                  value={user.email}
-                  onChange={handleInputs}
-                  placeholder="Email"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <p id="erroremail"></p>
-              <br></br>
-
-              <label className="custom-field two">
-                <input
-                  type="password"
-                  name="password"
-                  value={user.password}
-                  onChange={handleInputs}
-                  placeholder="Password"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <br></br>
-              <br/>
-              <label className="custom-field two"></label>
-
-              <label className="custom-field two">
-                <input
-                  type="text"
-                  name="district"
-                  value={user.district}
-                  onChange={handleInputs}
-                  placeholder="District"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-              <br></br>
-              <p id="errordistrict"></p>
-              <br></br>
-
-              <label className="custom-field two">
-                <input
-                  type="text"
-                  name="pincode"
-                  value={user.pincode}
-                  onChange={handleInputs}
-                  placeholder="Pincode"
-                  style={{ fontSize: "18px", color: "#77838F" }}
-                />
-              </label>
-            </div>
+        <div className="register-page-right">
+          <h2>REGISTER</h2>
+          <div className="register-form-wrapper">
+            <form id="reg-form" onSubmit={postData} method="POST" action="#">
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+              <div className="input-wrapper">
+                <label className="input-label" htmlFor="#"></label>
+                <input type="text" value="#" name="#" onChange="#" placeholder="#" />
+              </div>
+            </form>
           </div>
-          <div className="register-agree">
-            <p style={{ textDecoration: "underline", fontSize: "18px" }}>
-              <input type="checkbox" className="register-checkbox"></input>I
-              agree to Terms & Condition receiving marketing and promotional
-              materials.
-            </p>
-          </div>
-
-          <button
-            onClick={postData}
-            style={{
-              backgroundColor: "#5861AE",
-              marginLeft: "400px",
-              marginBottom: "10px",
-              fontSize: "larger",
-              fontWeight: 700,
-            }}
-            className="registerbtn"
-          >
-            Register
-          </button>
         </div>
       </div>
-      </div>
-      <FooterSend/>
+      <FooterSend />
     </>
   );
 };
